5c4d15252af31c0399d52f506c4d852ee3f68367
jlama <jels1988@gmail.com>
2015-09-21 10:42:00 -0500

    Creación de Proyecto, incluye Login y pantalla principal

---
+++ app/src/main/java/pe/lindley/tomapedidos/MainActivity.java
@@ -0,0 +1,74 @@
+package pe.lindley.tomapedidos;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout main_drawerlayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        main_drawerlayout = (DrawerLayout) findViewById(R.id.main_drawerlayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, main_drawerlayout,
+                R.string.app_name, R.string.app_name) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+        main_drawerlayout.setDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+}
---
+++ app/src/main/java/pe/lindley/tomapedidos/dao/ClienteDAO.java
@@ -0,0 +1,59 @@
+package pe.lindley.tomapedidos.dao;
+
+import android.database.Cursor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pe.lindley.tomapedidos.entities.Cliente;
+
+/**
+ * Created by jlama on 19/09/2015.
+ */
+public class ClienteDAO {
+    private static ClienteDAO INSTANCE = new ClienteDAO();
+
+    private ClienteDAO() {
+    }
+
+    public static ClienteDAO getInstance() {
+        if (INSTANCE == null) {
+            createInstance();
+        }
+        return INSTANCE;
+    }
+
+    private synchronized static void createInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new ClienteDAO();
+        }
+    }
+
+    public List<Cliente> getListaCliente() {
+        List<Cliente> lista = new ArrayList<>();
+        Cursor cursor = null;
+        try {
+            cursor = DataBaseHelper.myDataBase.query("TB_Cliente", null, null, null, null, null, null);
+
+            if (cursor.moveToFirst()) {
+                do {
+                    Cliente cliente = new Cliente();
+                    cliente.setClienteId(cursor.isNull(cursor.getColumnIndex("Cliente_Id")) ? 0 : cursor.getInt(cursor.getColumnIndex("Cliente_Id")));
+                    cliente.setClienteRazonSocial(cursor.isNull(cursor.getColumnIndex("Cliente_RazonSocial")) ? "" : cursor.getString(cursor.getColumnIndex("Cliente_RazonSocial")));
+                    cliente.setClienteNIT(cursor.isNull(cursor.getColumnIndex("Cliente_NIT")) ? "" : cursor.getString(cursor.getColumnIndex("Cliente_NIT")));
+                    cliente.setClienteDireccion(cursor.isNull(cursor.getColumnIndex("Cliente_Direccion")) ? "" : cursor.getString(cursor.getColumnIndex("Cliente_Direccion")));
+                    cliente.setClienteGiroNegocio(cursor.isNull(cursor.getColumnIndex("Cliente_GiroNegocio")) ? "" : cursor.getString(cursor.getColumnIndex("Cliente_GiroNegocio")));
+                    cliente.setClienteAltitud(cursor.isNull(cursor.getColumnIndex("Altitud")) ? "" : cursor.getString(cursor.getColumnIndex("Altitud")));
+                    cliente.setClienteLatitud(cursor.isNull(cursor.getColumnIndex("Latitud")) ? "" : cursor.getString(cursor.getColumnIndex("Latitud")));
+                    lista.add(cliente);
+                } while (cursor.moveToNext());
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return lista;
+    }
+}
